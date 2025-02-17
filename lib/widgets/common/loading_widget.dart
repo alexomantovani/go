f51920092dde5b964494fa95dffea3d20f546513
alexomantovani <oliveira.datacontrol@outlook.com>
2025-02-17 10:54:36 -0300

    feat: add Shimmer package, ShimmerLoading, modify LoadingWidget and modify its widget tests, add ShimmerLoading widget tests

--- lib/widgets/common/loading_widget.dart
+++ lib/widgets/common/loading_widget.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 
+import '../../core/extensions/context_ext.dart';
 import '../../core/services/styles.dart';
+import 'shimmer_loading.dart';
 
 class LoadingWidget extends StatelessWidget {
-  const LoadingWidget({
-    super.key,
-  });
+  const LoadingWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      key: const Key('Container'),
       decoration: BoxDecoration(
         color: Styles.kPrimaryPale,
         borderRadius: BorderRadius.only(
@@ -17,11 +18,38 @@ class LoadingWidget extends StatelessWidget {
           topRight: Radius.circular(24),
         ),
       ),
+      padding: EdgeInsets.all(12.0),
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Center(
-            child: const CircularProgressIndicator(),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              ShimmerLoading(
+                key: const Key('ShimmerLoading_Circle'),
+                circle: true,
+              ),
+              ShimmerLoading(
+                key: const Key('ShimmerLoading_Rectangle'),
+                height: context.height / 5,
+                width: context.width * 0.7,
+              ),
+            ],
+          ),
+          Expanded(
+            child: ListView.builder(
+              physics: NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              itemCount: 2,
+              itemBuilder: (context, index) => Padding(
+                padding: const EdgeInsets.only(top: 10.0),
+                child: ShimmerLoading(
+                  key: Key('ShimmerLoading_$index'),
+                  height: context.height / 3,
+                  width: context.width,
+                ),
+              ),
+            ),
           ),
         ],
       ),
